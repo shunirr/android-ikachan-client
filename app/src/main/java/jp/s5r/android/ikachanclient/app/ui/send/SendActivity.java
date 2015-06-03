ca4bb95ce953d8ef2c6ed26571f7babc42e91eaf
shunirr <m@s5r.jp>
2015-06-03 20:09:07 +0900

    アクションバー

--- app/src/main/java/jp/s5r/android/ikachanclient/app/ui/send/SendActivity.java
+++ app/src/main/java/jp/s5r/android/ikachanclient/app/ui/send/SendActivity.java
@@ -1,6 +1,7 @@
 package jp.s5r.android.ikachanclient.app.ui.send;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 
 import jp.s5r.android.ikachanclient.R;
 import jp.s5r.android.ikachanclient.app.ui.BaseActivity;
@@ -10,6 +11,14 @@ public class SendActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayShowTitleEnabled(false);
+            ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM, ActionBar.DISPLAY_SHOW_CUSTOM);
+            ab.setCustomView(R.layout.action_bar);
+        }
+
         addFragment(new SelectRoomFragment());
     }
 
